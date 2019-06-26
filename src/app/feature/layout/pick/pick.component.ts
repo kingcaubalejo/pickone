5cb2929b0404e54ca96b7e8989f8b3065433b73a
King Lawrence L. Caubalejo <king.caubalejo@valueline.com.ph>
2019-06-26 10:17:54 +0800

    Done with the desired features.

--- src/app/feature/layout/pick/pick.component.ts
+++ src/app/feature/layout/pick/pick.component.ts
@@ -17,34 +17,10 @@ export class PickComponent implements OnInit {
   @ViewChild('import_master_list') master_list
   students          = [];
   called_students   = [];
-//   students = [
-//     { 'Student_ID': 0, 'Student_Name': "bali-os, liezel joyce n." },
-//     { 'Student_ID': 1, 'Student_Name': "bolivar, princess" },
-//     { 'Student_ID': 2, 'Student_Name': "butlig, reziel r." },
-//     { 'Student_ID': 3, 'Student_Name': "caravana, leo jomar t." },
-//     { 'Student_ID': 4, 'Student_Name': "catanauan, gabriel paolo t." },
-//     { 'Student_ID': 5, 'Student_Name': "dela cruz, lydon" },
-//     { 'Student_ID': 6, 'Student_Name': "elarmo, jonelle g." },
-//     { 'Student_ID': 7, 'Student_Name': "macaya, rosella p." },
-//     { 'Student_ID': 8, 'Student_Name': "sarmiento, marikon d." },
-//     { 'Student_ID': 9, 'Student_Name': "silawan, joven erick f." },
-//     { 'Student_ID': 10, 'Student_Name': "sumang, liberty g." },
-//     { 'Student_ID': 11, 'Student_Name': "surban, kathleen" },
-//     { 'Student_ID': 12, 'Student_Name': "villanueva, melanie b." },
-//     { 'Student_ID': 13, 'Student_Name': "villareal, ma. jessa" }
-// ];
-
-  // display: String = document.getElementById("display_name").innerHTML;
 
   constructor() { }
 
   ngOnInit() {
-    console.log($("#master_list"), "jqueque");
-    this.uploader.onProgressItem = (progress: any) => { 
-      console.log(progress['progress']); }; 
-    this.uploader.onSuccessItem = (progress: any) => { 
-      console.log('I receive the response file posted in API'); 
-    };
   }
   
 
@@ -61,16 +37,14 @@ export class PickComponent implements OnInit {
     firstEl.style.color         = "black";
     firstEl.style.fontSize      = '1em';
     
-    var self            = this;
-    let total: number   = self.students.length;
-    let selected        = Math.floor(Math.random() * total);
-    let i: number       = 0;
-    var table_tbody     = "";
+    var self                    = this;
+    let total: number           = self.students.length;
+    let selected                = Math.floor(Math.random() * total);
+    let i: number               = 0;
+    var table_tbody_done        = "";
     
     $("#tbody_called_list").empty();
-    
-    console.log(this.students, "TEST");
-    // document.getElementById("display_name").innerHTML = "";
+
     for (i=0; i<total; i++) {
       setTimeout((function(i){
           return function(){
@@ -78,25 +52,23 @@ export class PickComponent implements OnInit {
               
               if(i === selected ) {
                 self.called_students.push(self.students[i].Student_Name);
-                var el = document.getElementById("display_name");
+
+                var el                    = document.getElementById("display_name");
                 el.innerHTML              = self.students[i].Student_Name.toUpperCase();
                 el.style.fontWeight       = 'bold';
                 firstEl.style.color       = "black";
                 el.style.fontSize         = '2em';
 
+                //put the called student(s) in the Done table
                 for(var j = 0; j < self.called_students.length; j++) {
-                  table_tbody += "<tr>";
-                    table_tbody += "<td>"+self.called_students[j].toUpperCase()+"</td>";
-                  table_tbody += "</tr>";
+                  table_tbody_done += "<tr style='display: block'>";
+                    table_tbody_done += "<td class='has-text-centered' style='display: block'>"+self.called_students[j].toUpperCase()+"</td>";
+                  table_tbody_done += "</tr>";
                 }
-                $("#tbody_called_list").append(table_tbody);
+                $("#tbody_called_list").append(table_tbody_done);
 
                 //animate it!
-                el.className += "animated ";
-                el.className += "infinite ";
-                el.className += "fadeIn";
-                console.log("Animate runs");
-                console.log(self.called_students, "Called");
+                el.className = "animated infinite fadeIn";
               }
           };
       }(i)), i*50);
@@ -121,49 +93,53 @@ export class PickComponent implements OnInit {
 
   arrayBuffer: any;
   onUploadFiles(event) {
-    let file: FileList = event.target.files;
-    var self = this;
+    let file: FileList      = event.target.files;
+    var self                = this;
+
     if(file) {
-      let f: File = file[0];
-      let fileReader: FileReader = new FileReader();
+      let f: File                   = file[0];
+      let fileReader: FileReader    = new FileReader();
 
       $("#tbody_master_list").empty();
 
       fileReader.onload = (e) => {
-        console.log(self.students, "LUMA");
-        this.arrayBuffer = fileReader.result;
-        var data = new Uint8Array(this.arrayBuffer);
-        var arr = new Array();
+
+        this.arrayBuffer      = fileReader.result;
+        var data              = new Uint8Array(this.arrayBuffer);
+        var arr               = new Array();
+        
         for (var i = 0; i != data.length; ++i) {
           arr[i] = String.fromCharCode(data[i]);
         }
-        var bstr = arr.join("");
-        var workbook = XLSX.read(bstr, {type:"binary"});
-        var first_sheet_name = workbook.SheetNames[0];
-        var worksheet = workbook.Sheets[first_sheet_name];
 
-        var names = XLSX.utils.sheet_to_json(worksheet,{raw:true})
-        var table_tbody = "";
+        var bstr                = arr.join("");
+        var workbook            = XLSX.read(bstr, {type:"binary"});
+        var first_sheet_name    = workbook.SheetNames[0];
+        var worksheet           = workbook.Sheets[first_sheet_name];
+
+        var names                     = XLSX.utils.sheet_to_json(worksheet,{raw:true})
+        var table_tbody               = "";
+        var table_tbody_importing     = "";
+
+        table_tbody_importing += "<tr style='display: block;'>";
+          table_tbody_importing += "<td class='has-text-danger' style='display: block;'>Importing...</td>";
+        table_tbody_importing += "</tr>";
+        $("#tbody_master_list").append(table_tbody_importing);
+
         for(var i = 0; i < names.length; i++) {
-          // console.log(names[i]["Name"], "test");
-          table_tbody += "<tr>";
-            table_tbody += "<td>"+names[i]["Student_Name"].toUpperCase()+"</td>";
+          table_tbody += "<tr style='display: block;'>";
+            table_tbody += "<td style='display: block;'>"+names[i]["Student_Name"].toUpperCase()+"</td>";
           table_tbody += "</tr>";
         }
 
+        let idleTime: number = 1000;
         setTimeout(function() {
-          console.log("START");
-          console.log($("#tbody_master_list"));
+          $("#tbody_master_list").empty();
           $("#tbody_master_list").append(table_tbody);
-          console.log("END");
-
           self.students = names;
-          console.log(self.students, "NEW STUDENT");
-        }, 1000);
-        // console.log(names, "TEST1");
+        }, idleTime);
       }
       fileReader.readAsArrayBuffer(f);
     }
-    console.log("TEST", file);
   }
 }
